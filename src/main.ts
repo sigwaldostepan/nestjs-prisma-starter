004ca8b5fb1e75b742837a2353e28ea135386f26
unknown <sigwaldos@gmail.com>
2025-02-23 16:42:38 +0700

    railway > vercel

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env.ALLOWED_ORIGIN);
+  app.enableCors({
+    origin: ['http://localhost:3000', process.env.ALLOWED_ORIGIN],
+  });
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
