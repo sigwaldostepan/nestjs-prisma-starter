8f59be1fc13c06691d6c1fe18df0f0ed8b05f1e8
unknown <sigwaldos@gmail.com>
2025-02-23 17:30:35 +0700

    port fix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(process.env.ALLOWED_ORIGIN);
   app.enableCors();
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 4000, '0.0.0.0');
 }
 bootstrap();
