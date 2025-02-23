8d3705c5ca3033e8c6f0d12cc4b014e53de06d9e
unknown <sigwaldos@gmail.com>
2025-02-23 17:13:46 +0700

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(process.env.ALLOWED_ORIGIN);
-  app.enableCors({
-    origin: ['http://localhost:3000', process.env.ALLOWED_ORIGIN],
-  });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
